e78d9efef365e1bcab1dcef14cfaf7a397c68846
QMENG <damon.q.meng@gmail.com>
2019-02-16 20:42:22 -0500

    able to read from temperature

--- SensorTagInteractionTrials/ViewController.swift
+++ SensorTagInteractionTrials/ViewController.swift
@@ -7,14 +7,134 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var centralManager : CBCentralManager!
+    var sensorTagPeripheral : CBPeripheral!
+    
+    let IRTemperatureServiceUUID = CBUUID(string: "F000AA00-0451-4000-B000-000000000000")
+    let IRTemperatureDataUUID   = CBUUID(string: "F000AA01-0451-4000-B000-000000000000")
+    let IRTemperatureConfigUUID = CBUUID(string: "F000AA02-0451-4000-B000-000000000000")
+    
+    let AccelerometerServiceUUID = CBUUID(string: "F000AA10-0451-4000-B000-000000000000")
+    let AccelerometerDataUUID   = CBUUID(string: "F000AA11-0451-4000-B000-000000000000")
+    let AccelerometerConfigUUID = CBUUID(string: "F000AA12-0451-4000-B000-000000000000")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
-
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            // Scan for peripherals if BLE is turned on
+            central.scanForPeripherals(withServices: nil, options: nil)
+            self.statusLabel.text = "Searching for BLE Devices"
+        }
+        else {
+            // Can have different conditions for all states if needed - print generic message for now
+            print("Bluetooth switched off or not initialized")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        
+        let deviceName = "CC2650 SensorTag"
+        let nameOfDeviceFound = (advertisementData as NSDictionary).object(forKey: CBAdvertisementDataLocalNameKey) as? String
+        
+        if (nameOfDeviceFound == deviceName) {
+            // Update Status Label
+            self.statusLabel.text = "Sensor Tag Found"
+            
+            // Stop scanning
+            self.centralManager.stopScan()
+            // Set as the peripheral to use and establish connection
+            self.sensorTagPeripheral = peripheral
+            self.sensorTagPeripheral.delegate = self
+            self.centralManager.connect(peripheral, options: nil)
+        }
+        else {
+            self.statusLabel.text = "Sensor Tag NOT Found"
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        self.statusLabel.text = "Discovering peripheral services"
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(central: CBCentralManager!, didDisconnect peripheral: CBPeripheral!, error: NSError!) {
+        self.statusLabel.text = "Disconnected"
+        central.scanForPeripherals(withServices: nil, options: nil)
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        self.statusLabel.text = "Looking at peripheral services"
+        for service in peripheral.services! {
+            let thisService = service as CBService
+            if service.uuid == AccelerometerServiceUUID {
+                // Discover characteristics of IR Temperature Service
+                peripheral.discoverCharacteristics(nil, for: thisService)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        
+        self.statusLabel.text = "Enabling sensors"
+        
+        var enableValue = 1
+        let enablyBytes = NSData(bytes: &enableValue, length: MemoryLayout<UInt8>.size)
+        
+        for charateristic in service.characteristics! {
+            let thisCharacteristic = charateristic as CBCharacteristic
+            if thisCharacteristic.uuid == AccelerometerDataUUID {
+                // Enable Sensor Notification
+                self.sensorTagPeripheral.setNotifyValue(true, for: thisCharacteristic)
+            }
+            if thisCharacteristic.uuid == AccelerometerConfigUUID {
+                // Enable Sensor
+                self.sensorTagPeripheral.writeValue(enablyBytes as Data, for: thisCharacteristic, type: CBCharacteristicWriteType.withResponse)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        
+        self.statusLabel.text = "Connected"
+        
+        if characteristic.uuid == AccelerometerDataUUID {
+            // Convert NSData to array of signed 16 bit values
+            let dataFromSensor = dataToSignedBytes8(value: characteristic.value! as NSData)
+            let xVal = Double(dataFromSensor[0]) / 64
+            let yVal = Double(dataFromSensor[1]) / 64
+            let zVal = Double(dataFromSensor[2]) / 64 * -1
+            
+            self.xLabel.text = String(format: "%.2f", xVal)
+            self.yLabel.text = String(format: "%.2f", yVal)
+            self.zLabel.text = String(format: "%.2f", zVal)
+        }
+    }
+    
+    func dataToSignedBytes16(value : NSData) -> [Int16] {
+        let count = value.length
+        var array = [Int16](repeating: 0, count: count)
+        value.getBytes(&array, length:count * MemoryLayout<Int16>.size)
+        return array
+    }
+    
+    func dataToSignedBytes8(value : NSData) -> [Int8] {
+        let count = value.length
+        var array = [Int8](repeating: 0, count: count)
+        value.getBytes(&array, length:count * MemoryLayout<Int8>.size)
+        return array
+    }
 }
 
